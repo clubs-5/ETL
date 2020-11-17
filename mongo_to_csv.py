fa268f34bcfaaa60a13b2da93b05ca497337d9c2
veritaspoutine <14975224+veritaspoutine@users.noreply.github.com>
2020-11-17 20:31:49 +0800

    finished list of tuples and fixed critic_ratings type, and created pandas dataframe

--- mongo_to_csv.py
+++ mongo_to_csv.py
@@ -1,9 +1,11 @@
 from pymongo import MongoClient
+import pandas as pd
 
 
 # Requires the PyMongo package.
 # https://api.mongodb.com/python/current
 
+#get desired json values
 client = MongoClient('mongodb://192.168.33.101:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
 result = client['TVShows']['ff'].aggregate([
     {
@@ -30,28 +32,39 @@ result = client['TVShows']['ff'].aggregate([
     }
 ])
 
-#print(type(result))
-
+show_tomato = []
+n =1
+#iterate through each tv show (document is this case) to create a list of tuples
 for i in result:
     x = 1
-    show_tomato = []
+    
     name = i['Show']
-    while i['tomato']:
 
-        if x < 21:
+
+    while i['Show']:
+
+        if x < 21: #iterate through each available season, assuming no more than 20 seasons
 
             try: 
-                if i['tomato']['Season {}'.format(x)]:
-                    print('Season {} exists'.format(x))
-                    print
-        
+                season = i['tomato']['Season {}'.format(x)]
+                if season:
+                   s =  int(season['Critic_Ratings']) #Becasue I forgot to turn critic rating into integer while crawling
+                   season_tuple = (name, x, season['User_ratings'], season['audience_score'], s, season['tomatometer'])
+                   show_tomato.append(season_tuple) 
+       
             except:
-                print('Season {} DNE'.format(x))
+                pass
 
         else:
             break
-        #x += 1
-        break
+        x += 1
+
+
+
+#now we create a pandas dataframe 
+df = pd.DataFrame(show_tomato, columns = ['Show', 'Season', 'User_Ratings', 'Audience_Score', 'Critic_Ratings', 'Tomatometer'])
+
+print(df)
 # with open('out.csv', 'a', encoding='utf8') as obj:
 #     for i in result:
 
