601341086faf9744c1948c15367868af3064ad0a
alexchen0228 <loveyung1124@gmail.com>
2021-01-14 01:50:32 -0800

    code refactoring1: film company ETL

--- deduplicate_reviews.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-FILES=/home/rubik/Documents/tibame/project/ETL/reviews/*.txt
-for f in $FILES
-  do 
-    #sort -u "$f" > "./fixed/fixed_$(basename "$f")"
-    sort -u "$f" > "./fixed/$(basename "$f")"
-  done
